f21a646a86c698d5ab5565b1a5275159378198da
Edmond Wong <edmonwong12@gmail.com>
2025-09-26 15:54:13 -0400

    initial commit

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <v-btn class="h-full" variant="outlined">HELLO</v-btn>
+    <v-btn class="w-full" variant="outlined">HELLO</v-btn>
+	<Button />
     <Sample />
   </div>
 </template>
 
 <script setup lang="ts">
+import Button from '@/components/Button.vue';
 import Sample from '@/components/Sample.vue';
 
 </script>
