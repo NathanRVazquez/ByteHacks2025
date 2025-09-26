7460100e95222bd345f2a8c41098ca37080832c2
SuperUltraMegaChicken <bobpost515@gmail.com>
2025-09-26 16:51:02 -0400

    Added navbar + icons

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
 <template>
-  <div>
+  <div class="ma-4">
+    <div class="mb-4 flex w-full justify-between">
+      <div class="flex gap-[10px]">
+        <router-link
+          v-for="page in leftPages"
+          :key="page.path"
+          :to="page.path"
+          class="p-4"
+        >
+          <v-btn color="primary">{{ page.title }}
+            <template #prepend>
+              <font-awesome-icon :icon="['fa', page.icon]" class="mr-2" />
+            </template>
+          </v-btn>
+        </router-link>
+      </div>
+      <div class="flex gap-[10px] justify-end flex-row-reverse">
+        <router-link
+          v-for="page in rightPages"
+          :key="page.path"
+          :to="page.path"
+          class="p-4"
+        >
+          <v-btn color="primary">{{ page.title }}
+            <template #prepend>
+              <font-awesome-icon :icon="['fa', page.icon]" class="mr-2" />
+            </template>
+          </v-btn>
+        </router-link>
+      </div>
+    </div>
     <router-view></router-view>
   </div>
 </template>
 
 <script setup lang="ts">
-
+const leftPages = [
+  { title: "Home", path: "/", icon: "fa-house" },
+]
+const rightPages = [
+  { title: "Home", path: "/", icon: "fa-house" },
+] 
 </script>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import router from './router'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, fa } from 'vuetify/iconsets/fa'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 import 'vuetify/styles'
 import '@fontsource/roboto/100.css'
 import '@fontsource/roboto/300.css'
@@ -19,10 +24,21 @@ const app = createApp(App)
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: {
+      fa,
+    },
+  },
   theme: {
     defaultTheme: "dark"
   }
 })
+
+app.component('font-awesome-icon', FontAwesomeIcon) // Register component globally
+library.add(fas) // Include needed solid icons
+library.add(far) // Include needed regular icons
 
 app.use(createPinia())
 app.use(router)
